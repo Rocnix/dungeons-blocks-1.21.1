b0690a18996e68259e82186dc5a59126103d5b41
Rocnix <55219363+Rocnix@users.noreply.github.com>
2024-11-26 22:13:11 -0800

    alphabetize and correct json typo

--- src/main/java/beasts/dungeonsblocks/block/ModBlocks.java
+++ src/main/java/beasts/dungeonsblocks/block/ModBlocks.java
@@ -14,19 +14,71 @@ public class ModBlocks {
 
 // ooo look at me, im learning how to comment <3
 
-
+    // simple cubes
+    public static final Block BLEACHED_STONE_BRICKS = registerBlock("bleached_stone_bricks",
+            new Block(AbstractBlock.Settings.create()
+                    .strength(4f)
+                    .requiresTool()
+                    .sounds(BlockSoundGroup.STONE) // defaults to stone
+            ));
+    public static final Block CARVED_PRISMARINE_BRICKS = registerBlock("carved_prismarine_bricks",
+            new Block(AbstractBlock.Settings.create()
+                    .strength(4f)
+                    .requiresTool()
+                    .sounds(BlockSoundGroup.STONE) // defaults to stone
+            ));
+    public static final Block CHISELED_BLEACHED_STONE_BRICKS = registerBlock("chiseled_bleached_stone_bricks",
+            new Block(AbstractBlock.Settings.create()
+                    .strength(4f)
+                    .requiresTool()
+                    .sounds(BlockSoundGroup.STONE) // defaults to stone
+            ));
+    public static final Block CHISELED_GRANITE_BRICKS = registerBlock("chiseled_granite_bricks",
+            new Block(AbstractBlock.Settings.create()
+                    .strength(4f)
+                    .requiresTool()
+                    .sounds(BlockSoundGroup.STONE) // defaults to stone
+            ));
+    public static final Block CRACKED_GRANITE_BRICKS = registerBlock("cracked_granite_bricks",
+            new Block(AbstractBlock.Settings.create()
+                    .strength(4f)
+                    .requiresTool()
+                    .sounds(BlockSoundGroup.STONE) // defaults to stone
+            ));
+    public static final Block DARK_PRISMARINE_FLOOR = registerBlock("dark_prismarine_floor",
+            new Block(AbstractBlock.Settings.create()
+                    .strength(4f)
+                    .requiresTool()
+                    .sounds(BlockSoundGroup.STONE) // defaults to stone
+            ));
+    public static final Block GRANITE_BRICKS = registerBlock("granite_bricks",
+            new Block(AbstractBlock.Settings.create()
+                    .strength(4f)
+                    .requiresTool()
+                    .sounds(BlockSoundGroup.STONE) // defaults to stone
+            ));
+    public static final Block MOSSY_CHISELED_BLEACHED_STONE_BRICKS = registerBlock("mossy_chiseled_bleached_stone_bricks",
+            new Block(AbstractBlock.Settings.create()
+                    .strength(4f)
+                    .requiresTool()
+                    .sounds(BlockSoundGroup.STONE) // defaults to stone
+            ));
     public static final Block PRISMARINE_SCALES_BLOCK = registerBlock("prismarine_scales",
         new Block(AbstractBlock.Settings.create()
             .strength(4f)
             .requiresTool()
             .sounds(BlockSoundGroup.STONE) // defaults to stone
         ));
-    public static final Block BLEACHED_STONE_BRICKS = registerBlock("bleached_stone_bricks",
-        new Block(AbstractBlock.Settings.create()
-            .strength(4f)
-            .requiresTool()
-            .sounds(BlockSoundGroup.STONE) // defaults to stone
-        ));
+    public static final Block SMALL_GRANITE_BRICKS = registerBlock("small_granite_bricks",
+            new Block(AbstractBlock.Settings.create()
+                    .strength(4f)
+                    .requiresTool()
+                    .sounds(BlockSoundGroup.STONE) // defaults to stone
+            ));
+
+
+    // complex cubes
+
     public static final Block PRISMARINE_BOOKSHELF = registerBlock("prismarine_bookshelf",
             new Block(AbstractBlock.Settings.create()
                     .strength(4f)
@@ -45,6 +97,15 @@ public class ModBlocks {
                     .requiresTool()
                     .sounds(BlockSoundGroup.STONE) // defaults to stone
             ));
+    public static final Block SMALL_PRISMARINE_BRICKS = registerBlock("small_prismarine_bricks",
+            new Block(AbstractBlock.Settings.create()
+                    .strength(4f)
+                    .requiresTool()
+                    .sounds(BlockSoundGroup.STONE) // defaults to stone
+            ));
+
+
+    // plants
     public static final Block SEALEAF = registerBlock("sealeaf",
             new Block(AbstractBlock.Settings.create()
                     .strength(4f)
@@ -57,18 +118,6 @@ public class ModBlocks {
                     .requiresTool()
                     .sounds(BlockSoundGroup.STONE) // defaults to stone
             ));
-    public static final Block SMALL_GRANITE_BRICKS = registerBlock("small_granite_bricks",
-            new Block(AbstractBlock.Settings.create()
-                    .strength(4f)
-                    .requiresTool()
-                    .sounds(BlockSoundGroup.STONE) // defaults to stone
-            ));
-    public static final Block SMALL_PRISMARINE_BRICKS = registerBlock("small_prismarine_bricks",
-            new Block(AbstractBlock.Settings.create()
-                    .strength(4f)
-                    .requiresTool()
-                    .sounds(BlockSoundGroup.STONE) // defaults to stone
-            ));
     public static final Block TALL_CORAL = registerBlock("tall_coral",
             new Block(AbstractBlock.Settings.create()
                     .strength(4f)
@@ -76,48 +125,6 @@ public class ModBlocks {
                     .sounds(BlockSoundGroup.STONE) // defaults to stone
             ));
     public static final Block TALL_SEALEAF = registerBlock("tall_sealeaf",
-            new Block(AbstractBlock.Settings.create()
-                    .strength(4f)
-                    .requiresTool()
-                    .sounds(BlockSoundGroup.STONE) // defaults to stone
-            ));
-    public static final Block CHISELED_BLEACHED_STONE_BRICKS = registerBlock("chiseled_bleached_stone_bricks",
-            new Block(AbstractBlock.Settings.create()
-                    .strength(4f)
-                    .requiresTool()
-                    .sounds(BlockSoundGroup.STONE) // defaults to stone
-            ));
-    public static final Block CHISELED_GRANITE_BRICKS = registerBlock("chiseled_granite_bricks",
-            new Block(AbstractBlock.Settings.create()
-                    .strength(4f)
-                    .requiresTool()
-                    .sounds(BlockSoundGroup.STONE) // defaults to stone
-            ));
-    public static final Block CARVED_PRISMARINE_BRICKS = registerBlock("carved_prismarine_bricks",
-            new Block(AbstractBlock.Settings.create()
-                    .strength(4f)
-                    .requiresTool()
-                    .sounds(BlockSoundGroup.STONE) // defaults to stone
-            ));
-    public static final Block CRACKED_GRANITE_BRICKS = registerBlock("cracked_granite_bricks",
-            new Block(AbstractBlock.Settings.create()
-                    .strength(4f)
-                    .requiresTool()
-                    .sounds(BlockSoundGroup.STONE) // defaults to stone
-            ));
-    public static final Block GRANITE_BRICKS = registerBlock("granite_bricks",
-            new Block(AbstractBlock.Settings.create()
-                    .strength(4f)
-                    .requiresTool()
-                    .sounds(BlockSoundGroup.STONE) // defaults to stone
-            ));
-    public static final Block MOSSY_CHISELED_BLEACHED_STONE_BRICKS = registerBlock("mossy_chiseled_bleached_stone_bricks",
-            new Block(AbstractBlock.Settings.create()
-                    .strength(4f)
-                    .requiresTool()
-                    .sounds(BlockSoundGroup.STONE) // defaults to stone
-            ));
-    public static final Block DARK_PRISMARINE_FLOOR = registerBlock("dark_prismarine_floor",
             new Block(AbstractBlock.Settings.create()
                     .strength(4f)
                     .requiresTool()
--- src/main/java/beasts/dungeonsblocks/datagen/ModModelProvider.java
+++ src/main/java/beasts/dungeonsblocks/datagen/ModModelProvider.java
@@ -16,24 +16,29 @@ public class ModModelProvider extends FabricModelProvider {
 // testing a comment while working on things. how does this work out?
     @Override
     public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.PRISMARINE_SCALES_BLOCK);
+        // simple cubes
         blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.BLEACHED_STONE_BRICKS);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.PRISMARINE_BOOKSHELF);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.PRISMARINE_KEYSTONE);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.PRISMARINE_PILLAR);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.SEALEAF);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.SEALEAF_VINE);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.SMALL_GRANITE_BRICKS);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.SMALL_PRISMARINE_BRICKS);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.TALL_CORAL);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.TALL_SEALEAF);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.CHISELED_BLEACHED_STONE_BRICKS);
         blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.CARVED_PRISMARINE_BRICKS);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.CHISELED_BLEACHED_STONE_BRICKS);
         blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.CHISELED_GRANITE_BRICKS);
         blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.CRACKED_GRANITE_BRICKS);
         blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.DARK_PRISMARINE_FLOOR);
         blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.GRANITE_BRICKS);
         blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.MOSSY_CHISELED_BLEACHED_STONE_BRICKS);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.PRISMARINE_SCALES_BLOCK);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.SMALL_GRANITE_BRICKS);
+
+        // complex cubes
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.PRISMARINE_BOOKSHELF);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.PRISMARINE_KEYSTONE);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.PRISMARINE_PILLAR);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.SMALL_PRISMARINE_BRICKS);
+
+        // plants
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.SEALEAF);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.SEALEAF_VINE);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.TALL_CORAL);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.TALL_SEALEAF);
     }
 
     @Override
