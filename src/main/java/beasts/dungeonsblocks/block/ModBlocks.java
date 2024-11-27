2456fededda1342dc11491069bfa877561cb75f9
xxenobiology <xxenobiology@gmail.com>
2024-11-26 22:02:00 -0600

    Test comment

--- src/main/java/beasts/dungeonsblocks/block/ModBlocks.java
+++ src/main/java/beasts/dungeonsblocks/block/ModBlocks.java
@@ -14,6 +14,8 @@ import net.minecraft.util.Identifier;
 
 public class ModBlocks {
 
+// ooo look at me, im learning how to comment <3
+
 //    public static final Block PINK_GARNET_BLOCK = registerBlock("pink_garnet_block",
 //            new Block(AbstractBlock.Settings.create()
 //                    .strength(4f)
